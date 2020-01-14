9f53427f2ea7a79331059cdf7b1ee4b94fbbbe07
Sotiris Niarchos <sot.niarchos@gmail.com>
2020-01-14 03:16:34 +0200

    created an "automated" type support mechanism; added more types

--- utils/src/typegen.cpp
+++ utils/src/typegen.cpp
@@ -1,8 +1,47 @@
 #include "typegen.hpp"
 
+/*
+ * To add a new OpenCL primitive type,
+ * just add the type name (without "cl_") in the TYPELIST below, in the proper way (delims between types)
+ * !!! BE WARNED !!! This source code is NOT guaranteed to compile on every machine out there:
+ * cl_bool and cl_ushort are emitted due to collisions with other types, and some of the existing ones
+ * may lead to unpredicted collisions based on the underlying OS/hardware.
+ * So, edit TYPELIST until it compiles and proceed with caution... :)
+ */
+
+#define CL(X) cl_ ## X
+
+// NOTE: cl_bool is the same as cl_uint (that is, unsigned int), so it is emitted from limits_t
+// NOTE: cl_ushort breaks the helpmap, because ushort is basically half
+
+#define TYPELIST EACH(char)   \
+                 DELIM        \
+                 EACH(uchar)  \
+                 DELIM        \
+                 EACH(short)  \
+                 DELIM        \
+                 EACH(int)    \
+                 DELIM        \
+                 EACH(uint)   \
+                 DELIM        \
+                 EACH(long)   \
+                 DELIM        \
+                 EACH(ulong)  \
+                 DELIM        \
+                 EACH(float)  \
+                 DELIM        \
+                 EACH(double) \
+                 DELIM        \
+                 EACH(half)
+
 namespace typegen {
 
-typedef std::variant<std::pair<cl_int, cl_int>, std::pair<cl_uint, cl_uint>, std::pair<cl_float, cl_float>> limits_t;
+#define EACH(X) std::pair<CL(X), CL(X)>
+#define DELIM ,
+typedef std::variant<TYPELIST> limits_t;
+#undef EACH
+#undef DELIM
+
 typedef void (*genfunc_t) (void *, size_t, limits_t);
 
 template <typename T>
@@ -11,17 +50,18 @@ void rand_fill(void *, size_t, limits_t);
 class typeinfo_t {
 
 private:
-	std::unordered_map< std::string, std::tuple<size_t, genfunc_t, limits_t> > helpmap =
-	{
-		{ "int",   { sizeof(cl_int),   rand_fill<cl_int>,   std::pair<cl_int, cl_int>(0, 0)         } },
-		{ "uint",  { sizeof(cl_uint),  rand_fill<cl_uint>,  std::pair<cl_uint, cl_uint>(0u, 0u)     } },
-		{ "float", { sizeof(cl_float), rand_fill<cl_float>, std::pair<cl_float, cl_float>(.0f, .0f) } }
-	};
+
+#define EACH(X) { #X, { sizeof(CL(X)), rand_fill<CL(X)>, std::pair<CL(X), CL(X)>(0, 0) } }
+#define DELIM ,
+    std::unordered_map< std::string, std::tuple<size_t, genfunc_t, limits_t> > helpmap = { TYPELIST };
+#undef EACH
+#undef DELIM
 
 public:
-	size_t    get_size    (std::string typestr) { return std::get<0>(helpmap[typestr]); };
-	genfunc_t get_genfunc (std::string typestr) { return std::get<1>(helpmap[typestr]); };
-	limits_t& get_limits  (std::string typestr) { return std::get<2>(helpmap[typestr]); };
+
+    size_t    get_size    (std::string typestr) { return std::get<0>(helpmap[typestr]); };
+    genfunc_t get_genfunc (std::string typestr) { return std::get<1>(helpmap[typestr]); };
+    limits_t& get_limits  (std::string typestr) { return std::get<2>(helpmap[typestr]); };
 
 };
 
@@ -44,10 +84,10 @@ typename std::enable_if
 
 template <typename T>
 void generate_rand_values(T& a, limits_t limits) {
-	T l, h;
-	auto limits_pair = std::get<std::pair<T, T>>(limits);
-	l = limits_pair.first;
-	h = limits_pair.second;
+    T l, h;
+    auto limits_pair = std::get<std::pair<T, T>>(limits);
+    l = limits_pair.first;
+    h = limits_pair.second;
     uniform_distribution<T> d(l, h);
     a = d(gen);
 }
@@ -56,23 +96,35 @@ template <typename T>
 void rand_fill(void * buf, size_t nmemb, limits_t limits) {
     T * trubuf = (T *)buf;
     for(size_t i = 0; i < nmemb; i++)
-    	generate_rand_values<T>(trubuf[i], limits);
+        generate_rand_values<T>(trubuf[i], limits);
     return;
 }
 
-void set_limits(int size) {
-	std::get<std::pair<cl_int, cl_int>>(typeinfo.get_limits("int"))       = { -size, size };
-	std::get<std::pair<cl_uint, cl_uint>>(typeinfo.get_limits("uint"))    = { 0u, (cl_uint)size };
-	std::get<std::pair<cl_float, cl_float>>(typeinfo.get_limits("float")) = { (cl_float)-size, (cl_float)size };
+#define MAX(X, Y) (X > Y ? X : Y)
+#define MIN(X, Y) (X < Y ? X : Y)
+/*
+ * min limit -> the max between the type min value and -size
+ * max limit -> the min between the type max value and size
+ */
+#define EACH(X) std::get<std::pair<CL(X), CL(X)>>(typeinfo.get_limits(#X)) = \
+{                                                                            \
+    ( std::numeric_limits<CL(X)>::min() == 0 ? 0 :                           \
+        (CL(X))MAX(std::numeric_limits<CL(X)>::min(), -size)                 \
+    ),                                                                       \
+    ( (CL(X))MIN((long long)std::numeric_limits<CL(X)>::max(), size) )       \
 }
+#define DELIM ;
+void set_limits(int size) { TYPELIST; }
+#undef EACH
+#undef DELIM
 
 std::pair<void*, size_t> generate_kernel_argument(std::string typestr, size_t nmemb) {
-	size_t    typesize    = typeinfo.get_size(typestr);
-	limits_t  typelimits  = typeinfo.get_limits(typestr);
-	genfunc_t typegenfunc = typeinfo.get_genfunc(typestr);
-	void *    buf         = malloc(typesize * nmemb);
-	typegenfunc(buf, nmemb, typelimits);
-	return std::make_pair(buf, typesize * nmemb);
+    size_t    typesize    = typeinfo.get_size(typestr);
+    limits_t  typelimits  = typeinfo.get_limits(typestr);
+    genfunc_t typegenfunc = typeinfo.get_genfunc(typestr);
+    void *    buf         = malloc(typesize * nmemb);
+    typegenfunc(buf, nmemb, typelimits);
+    return std::make_pair(buf, typesize * nmemb);
 }
 
 } // end namespace typegen
