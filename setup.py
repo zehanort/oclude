47ae99493a79a3f7662eb59d47ac7a298e0d7c57
Sotiris Niarchos <sot.niarchos@gmail.com>
2020-03-19 04:11:37 +0200

    prettified setup

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='oclude',
-    version='0.9',
-    description='OpenCL Universal Driving Environment',
-    long_description='An OpenCL driver to test and run standalone kernels on arbitrary devices',
-    author='Sotiris Niarchos',
-    author_email='sot.niarchos@gmail.com',
-    url='https://github.com/zehanort/oclude',
+    name =              'oclude',
+    version =           '0.9',
+    description =       'OpenCL Universal Driving Environment',
+    long_description =  'An OpenCL driver to test and run standalone kernels on arbitrary devices',
+    author =            'Sotiris Niarchos',
+    author_email =      'sot.niarchos@gmail.com',
+    url =               'https://github.com/zehanort/oclude',
 
-    py_modules=['oclude'],
-    install_requires=[
-        'argparse',
-        'pycparserext',
-        'pycparser'
-    ],
-    entry_points={
-        'console_scripts': ['oclude=oclude:run']
-    },
-    packages=['utils'],
+    py_modules =        ['oclude'],
+    install_requires =  ['argparse', 'pycparserext', 'pycparser'],
+    entry_points =      { 'console_scripts': ['oclude=oclude:run'] },
+    packages =          ['utils']
 )
