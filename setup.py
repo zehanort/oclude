d54eabb91b881e3aed9142282c491dd2976c5835
Sotiris Niarchos <sot.niarchos@gmail.com>
2020-03-24 14:43:48 +0200

    completely rewrote hostcode parser in Python with PyOpenCL

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from distutils.core import setup
 
 setup(
     name =             'oclude',
-    version =          '0.9',
+    version =          '0.9.1',
     description =      'OpenCL Universal Driving Environment',
     long_description = 'An OpenCL driver to test and run standalone kernels on arbitrary devices',
     author =           'Sotiris Niarchos',
     author_email =     'sot.niarchos@gmail.com',
     url =              'https://github.com/zehanort/oclude',
 
-    py_modules =       ['oclude'],
-    install_requires = ['pycparserext==2020.1'],
-    entry_points =     { 'console_scripts': ['oclude=oclude:run'] },
-    packages =         ['utils']
+    install_requires = ['pycparserext==2020.1', 'pyopencl', 'rvg'],
+    entry_points =     { 'console_scripts': ['oclude=oclude:run'] }
 )
