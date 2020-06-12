182143ce803c737ed32fe9135111879f9b0548f8
Sotiris Niarchos <sot.niarchos@gmail.com>
2020-06-12 22:25:29 +0300

    update tests for module usage

--- tests/testutils.py
+++ tests/testutils.py
@@ -27,9 +27,43 @@ def run_kernel_from_module(kernelfile, kernel):
     from oclude import profile_opencl_kernel
     from oclude.utils.constants import llvm_instructions
     kernelfilepath = os.path.join(testdir, kernelfile)
-    res = profile_opencl_kernel(kernelfilepath, kernel, 1024, 128, instcounts=True, timeit=True)
 
-    assert 'instcounts' in res
-    assert 'timeit' in res
-    assert sorted(list(res['instcounts'].keys())) == sorted(llvm_instructions)
-    assert all(x in res['timeit'] for x in ['hostcode', 'device', 'transfer'])
+    # case 1: neither instcounts nor timeit, samples = 1
+    res = profile_opencl_kernel(file=kernelfilepath, kernel=kernel, gsize=1024, lsize=128)
+    assert all(x in res for x in ['original file', 'instrumented file', 'kernel', 'results'])
+    assert res['results'] is None
+
+    # case 2: neither instcounts nor timeit, samples > 1
+    res = profile_opencl_kernel(file=kernelfilepath, kernel=kernel, gsize=1024, lsize=128, samples=10)
+    assert all(x in res for x in ['original file', 'instrumented file', 'kernel', 'results'])
+    assert res['results'] is None
+
+    # case 3: only instcounts, samples = 1
+    res = profile_opencl_kernel(file=kernelfilepath, kernel=kernel, gsize=1024, lsize=128, instcounts=True)
+    assert all(x in res for x in ['original file', 'instrumented file', 'kernel', 'results'])
+    assert len(res['results']) == 1
+    assert sorted(list(res['results'][0]['instcounts'].keys())) == sorted(llvm_instructions)
+    assert all(isinstance(x, int) for x in res['results'][0]['instcounts'].values())
+
+    # case 4: only timeit, samples = 1
+    res = profile_opencl_kernel(file=kernelfilepath, kernel=kernel, gsize=1024, lsize=128, timeit=True)
+    assert all(x in res for x in ['original file', 'instrumented file', 'kernel', 'results'])
+    assert len(res['results']) == 1
+    assert all(x in res['results'][0]['timeit'] for x in ['hostcode', 'device', 'transfer'])
+
+    # case 5: both instcounts and timeit, samples = 1
+    res = profile_opencl_kernel(file=kernelfilepath, kernel=kernel, gsize=1024, lsize=128, instcounts=True, timeit=True)
+    assert all(x in res for x in ['original file', 'instrumented file', 'kernel', 'results'])
+    assert len(res['results']) == 1
+    assert sorted(list(res['results'][0]['instcounts'].keys())) == sorted(llvm_instructions)
+    assert all(isinstance(x, int) for x in res['results'][0]['instcounts'].values())
+    assert all(x in res['results'][0]['timeit'] for x in ['hostcode', 'device', 'transfer'])
+
+    # case 6: both instcounts and timeit, samples > 1
+    res = profile_opencl_kernel(file=kernelfilepath, kernel=kernel, gsize=1024, lsize=128, instcounts=True, timeit=True, samples=10)
+    assert all(x in res for x in ['original file', 'instrumented file', 'kernel', 'results'])
+    assert len(res['results']) == 10
+    for results in res['results']:
+        assert sorted(list(results['instcounts'].keys())) == sorted(llvm_instructions)
+        assert all(isinstance(x, int) for x in results['instcounts'].values())
+        assert all(x in results['timeit'] for x in ['hostcode', 'device', 'transfer'])
